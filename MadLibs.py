a5484574c2fb7f9468e508fd893fbf03d3a4a5aa
tatianamelinti <39564785+tatianamelinti@users.noreply.github.com>
2020-06-18 00:31:12 +0300

    Update MadLibs.py

--- MadLibs.py
+++ MadLibs.py
@@ -3,9 +3,9 @@
 STORY = "This morning %s woke up feeling %s. It is going to be a %s day! Outside, a bunch of %s were protesting to keep %s in stores. They began to %s to the rhythm of the %s, which made all the %s very %s. Concerned, %s texted %s, who flew %s to %s and dropped %s in a puddle of frozen %s. %s woke up in the year %s, in a world where %s ruled the world."
 print ("Mad Libs has started")
 name = input("Enter a name: ")
-adj1 = input("Enter a adjectiv: ")
-adj2 = input("Enter a adjectiv: ")
-adj3 = input("Enter a adjectiv: ")
+adj1 = input("Enter an adjectiv: ")
+adj2 = input("Enter an adjectiv: ")
+adj3 = input("Enter an adjectiv: ")
 verb = input("Enter a verb: ")
 noun1 = input("Enter a noun: ")
 noun2 = input("Enter a noun: ")
